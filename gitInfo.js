1b4633c70aeb6e5d6dc40f5b5c71144134f06f46
Noah Clark <noahammon00@gmail.com>
2023-02-17 11:39:53 -0700

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,91 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = 'Git is a software that allows for version control and backing up of your files.';
+console.log(gitDefinition);
+console.log('--------');
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = 'GitHub is a website that in conjnction with the git software will store snapshots of your repostitory on the web.';
+console.log(gitHubDefinition);
+console.log('--------');
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = 'git init is a command used to put git into a folder to start tracking your files in said folder.';
+console.log(gitInitDefinition);
+console.log('--------');
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = 'git clone is used to clone (or make a copy of) a git repostitory.';
+console.log(gitCloneDefinition);
+console.log('--------');
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = 'git status is used to determine out of the files in a repo, which files have been modified or added and if they are ready to be committed.';
+console.log(gitStatusDefinition);
+console.log('--------');
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = 'git add is used to add files in a repo to the "stage" to be prepared for a commit.';
+console.log(gitAddDefinition);
+console.log('--------');
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = 'git commit is used to take a snapshot of your code and back it up, allowing you also to attach a message to the commit to explain what is new.';
+console.log(gitCommitDefinition);
+console.log('--------');
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = 'git push is used to add the commits youve made onto your github';
+console.log(gitPushDefinition);
